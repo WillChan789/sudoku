15900dd856b7929a9c7ce8b8115966ff26ff6114
William Chan <35630333+WillChan789@users.noreply.github.com>
2020-01-12 23:58:07 -0700

    check box

--- s.py
+++ s.py
@@ -40,5 +40,15 @@ def checkvalid(b, n, pos):
         if b[i][pos[1]] == n and pos[0] != i:
             return False
 
-    
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if b[i][j] == n and (i,j) != pos:
+                return False
+
+    return True
+
+
 printboard(board)
